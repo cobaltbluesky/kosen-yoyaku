fe1ee81d5e7ef424e8fc87a17fce97e3ead4d20b
gunjou <kosentaguri1@gmail.com>
2020-10-29 02:47:25 +0900

    db保存がうまくいかない

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './App.css';
 import firebase from './Firebase';
 
 const App=()=>{
+  //db
   const db=firebase.firestore();
-  const sendRequest=()=>{
-    db.collection("users").add({
-      first: "Ada",
-      last: "Lovelace",
-      born: 1815
+  //state
+  const [numberofPeople,setNumberofPeople]=useState(0);
+  const [name,setName]=useState("名無し");
+  const handleNameChange=(e)=>{
+    setName(e.target.value);
+  }
+  const handleChange=(e)=>{
+    setNumberofPeople(e.target.value);
+  }
+  const onSubmit=()=>{
+    db.collection("timetable").add({
+      name:name,
+      numofpeople:numberofPeople
     })
     .then(function(docRef) {
         console.log("Document written with ID: ", docRef.id);
@@ -17,8 +26,25 @@ const App=()=>{
         console.error("Error adding document: ", error);
     });
   }
+
   return (
-    <button type="button" onClick={sendRequest}>a</button>
+    <form onSubmit={onSubmit}>
+      代表者氏名を入力してください:
+      <textarea
+        value={name}
+        onChange={handleNameChange}/>
+      <select
+        value={numberofPeople}
+        onChange={handleChange}
+      >
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+      </select>人
+      <button type="submit">送信</button>
+    </form>
   );
 }
 
